9fbaeb81d56c03c035e671e03ba8ed27b72b385f
salko-ua <github@salko-ua.de>
2026-03-06 20:00:35 +0200

    added svg page and fixed background for phone devices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,13 +16,13 @@
       muted
       loop
       playsinline
-      class="absolute z-0 left-1/2 -translate-x-1/2 h-full w-[1920px] max-2xl:object-cover object-top"
+      class="absolute z-0 left-1/2 -translate-x-1/2 w-[1920px] h-[1080px] object-top max-2xl:object-cover"
     >
       <source src={videoSrc} type="video/mp4" />
     </video>
   {:else}
     <div
-      class=" absolute z-0 left-1/2 -translate-x-1/2 h-full w-[1920px] bg-top bg-no-repeat"
+      class="absolute z-0 left-1/2 -translate-x-1/2 h-full w-[1920px] bg-top bg-no-repeat"
       style="background-image: url({src})"
     ></div>
   {/if}
@@ -35,11 +35,13 @@
     <div
       class=" p-3 h-fit text-white min-h-10 w-full bg-black/40 backdrop-blur-sm col-span-1 lg:col-span-2 max-lg:order-2"
     >
-      <img
-        class=""
-        src="https://raw.githubusercontent.com/salko-ua/salko-ua/main/dark_mode.svg"
-        alt="Dark Mode Toggle"
-      />
+      <a href="/github_svg">
+        <img
+          class=""
+          src="https://raw.githubusercontent.com/salko-ua/salko-ua/main/dark_mode.svg"
+          alt="Dark Mode Toggle"
+        />
+      </a>
     </div>
     <SideMenu />
   </div>
---
+++ src/routes/github_svg/+page.svelte
@@ -0,0 +1,5 @@
+<img
+  class="w-screen"
+  src="https://raw.githubusercontent.com/salko-ua/salko-ua/main/dark_mode.svg"
+  alt="Dark Mode Toggle"
+/>
